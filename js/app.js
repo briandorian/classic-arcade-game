b169a7d985520342d764d0ba3342a22f9a96c12c
Brian Acosta <bacosta70@gmail.com>
2018-11-16 20:03:39 +0100

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(xPosition,yPosition,speed) {
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // We setup the initial x,y position
+    this.X = xPosition;
+    this.Y = yPosition;
+    // Setup of the initial speed.
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +21,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.X = (X + this.speed) * dt;
+
+      /* Updates the Enemy location (you need to implement)
+Handles collision with the Player (you need to implement)*/
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +33,58 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var Player = function(xPosition,yPosition,speed) {
+
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+
+    // We setup the initial x,y position
+    this.X = xPosition;
+    this.Y = yPosition;
+    // Setup of the initial speed.
+    this.speed = speed;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    parent.X = (parent.X + this.speed) * dt;
+    
+      /* Updates the Enemy location (you need to implement)
+Handles collision with the Player (you need to implement)*/
+
+
+
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+const player = new Player(0,0,1);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const allEnemies = [];
+for (const i=0 ; i > 3 ; i++){
+  allEnemies.push(new Enemy(i,0,1));
+    for (const z=0 ; z < 3; z++ ){
+      allEnemies.push(0,z,1);
+    }
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
